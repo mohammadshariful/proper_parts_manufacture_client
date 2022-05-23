f950dbc0357b7ea2c1c34e529a6a18ca931fa6a5
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-23 12:42:13 +0600

    socail login implement with firebase

--- src/pages/Shared/SocialLogin/SocialLogin.js
+++ src/pages/Shared/SocialLogin/SocialLogin.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import Google from "../../../assets/icons/google.png";
+import auth from "../../../Firebase/Firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (user) {
+      navigate(from, { replace: true });
+    }
+  }, [user, navigate, from]);
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <div>
+    <div className="text-center">
+      {error && <p className="text-error">{error.message}</p>}
       <div className="divider">OR</div>
       <div className="bg-primary text-white rounded-lg py-2">
-        <button className="flex items-center mx-auto ">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="flex items-center mx-auto "
+        >
           <img src={Google} alt="google" />
           <span className="mx-3">Continue with Google</span>
         </button>
