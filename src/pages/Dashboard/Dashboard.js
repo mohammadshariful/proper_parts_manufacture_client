8d4bf3bc16747112e1b96f9f29ae6377aff58bd5
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-24 11:17:32 +0600

    check the user admin or not if user admin show some routes and other routes didn't show

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
+import auth from "../../Firebase/Firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 
 const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin] = useAdmin(user);
   return (
     <div className="drawer overflow-auto drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
@@ -15,27 +20,35 @@ const Dashboard = () => {
       <div className="drawer-side">
         <label for="dashboard-sidebar" className="drawer-overlay"></label>
         <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-          <li>
-            <Link to="/dashboard">My Orders</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/review">Add A Review</Link>
-          </li>
+          {!admin && (
+            <>
+              <li>
+                <Link to="/dashboard">My Orders</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/review">Add A Review</Link>
+              </li>
+            </>
+          )}
           <li>
             <Link to="/">My Profile</Link>
           </li>
-          <li>
-            <Link to="/">Manage All Orders</Link>
-          </li>
-          <li>
-            <Link to="/">Add A Product</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/admin">Make Admin</Link>
-          </li>
-          <li>
-            <Link to="/">Manage Products</Link>
-          </li>
+          {admin && (
+            <>
+              <li>
+                <Link to="/">Manage All Orders</Link>
+              </li>
+              <li>
+                <Link to="/">Add A Product</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/admin">Make Admin</Link>
+              </li>
+              <li>
+                <Link to="/">Manage Products</Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
